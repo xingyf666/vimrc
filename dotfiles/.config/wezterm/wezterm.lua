9ced23cb1dfe11caf65e9f57881dab4559e4c9ff
archibate <1931127624@qq.com>
2026-01-25 12:28:52 +0800

    add i3 and wezterm

---
+++ dotfiles/.config/wezterm/wezterm.lua
@@ -0,0 +1,64 @@
+local wezterm = require 'wezterm'
+local config = {}
+if wezterm.config_builder then
+    config = wezterm.config_builder()
+end
+
+config.font = wezterm.font('JetBrainsMono Nerd Font')
+config.font_size = 22.0
+
+if false then
+    config.color_scheme = 'Tokyo Night'
+else
+    config.color_scheme = 'Midsummer Night'
+    config.colors = {
+        foreground = '#C6B8B1',
+        background = '#1C1E26',
+        cursor_fg = '#1C1E26',
+        cursor_bg = '#D34C6B',
+        cursor_border = '#D34C6B',
+        selection_fg = '#C6B8B1',
+        selection_bg = '#2E303E',
+        ansi = {
+            '#000000',
+            '#D85069',
+            '#2DCBBE',
+            '#E5A382',
+            '#35A5BB',
+            '#D34C68',
+            '#2DCBBE',
+            '#B1C7C9',
+        },
+        brights = {
+            '#666666',
+            '#D85069',
+            '#2DCBBE',
+            '#E5A382',
+            '#35A5BB',
+            '#D34C68',
+            '#2DCBBE',
+            '#B1C7C9',
+        },
+        tab_bar = {
+            active_tab = {
+                bg_color = '#B1C7C9',
+                fg_color = '#1C1E26',
+            },
+            inactive_tab = {
+                bg_color = '#1C1E26',
+                fg_color = '#879596',
+            },
+        },
+    }
+end
+
+config.window_background_opacity = 1.0
+config.scrollback_lines = 2000
+config.enable_scroll_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+config.exit_behavior = 'Close'
+
+config.initial_cols = 80
+config.initial_rows = 24
+
+return config
